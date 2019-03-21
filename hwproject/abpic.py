1bbe1f3fc6c898106904d4a890035d092b8222a6
Fang Wenbing <xhdzz0420@mail.ustc.edu.cn>
2019-03-21 21:58:35 +0800

    write GUI and adding pictures from labtop or internet

--- hwproject/abpic.py
+++ hwproject/abpic.py
@@ -4,13 +4,29 @@
 import sys
 import cv2 as cv
 import numpy as np
-from selfpackage import pwindow
+from selfpackage import pwindow, cwindow
 from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtWidgets import (QApplication, QDialog, QFileDialog,
     QGridLayout, QLabel, QPushButton, QWidget, QToolTip)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+
+    # 定义父窗口和子窗口
     p = pwindow()
-    # w.initUI()
+    c = cwindow()
+
+    # 按下add1按钮时从本地添加图片
+    p.abtn.clicked.connect(c.handle_click)
+    p.abtn.clicked.connect(c.openImg)
+
+    # 按下add2按钮时从网络添加图片
+    p.adbtn.clicked.connect(c.handle_click)
+    p.adbtn.clicked.connect(c.download_img)
+
+    # 隐藏父窗口
+    p.abtn.clicked.connect(p.hide)
+    p.adbtn.clicked.connect(p.hide)
+    # c.qbtn.clicked.connect(p.show)
+
     sys.exit(app.exec_())
--- hwproject/selfpackage.py
+++ hwproject/selfpackage.py
@@ -4,12 +4,14 @@
 import sys
 import cv2 as cv
 import numpy as np
+import requests
 from PyQt5.QtGui import QImage, QPixmap, QIcon, QFont
 from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtWidgets import (QApplication, QDialog, QFileDialog,
-    QGridLayout, QLabel, QPushButton, QWidget, QToolTip, QMessageBox)
+    QGridLayout, QLabel, QPushButton, QWidget, QToolTip, QMessageBox,
+    QInputDialog, QLineEdit)
 
-# window类继承于QWidget
+# pwindow类继承于QWidget
 class pwindow(QWidget):
 
     def __init__(self):
@@ -17,29 +19,30 @@ class pwindow(QWidget):
         # self.init_show()
         self.initUI()
 
-        # self.initButton()
-
-    # def show(self):
-    #     self.setWindowTitle('photo processing tool')
-    #     self.resize(1000,800)
-
     # 初始化窗口函数
     def initUI(self):
 
         # 窗口大小，位置，名称，图标
-        self.setGeometry(300, 300, 800, 600)
+        self.setGeometry(300, 300, 600, 400)
         self.setWindowTitle('photo processing tool')
         self.setWindowIcon(QIcon('pic.png'))
         self.show()
 
         # 为tooltip设置了10px的Sanserif字体
-        QToolTip.setFont(QFont('SansSerif', 20))
+        QToolTip.setFont(QFont('SansSerif', 15))
 
         # 创建按钮和提示消息
-        abtn = QPushButton('Add', self)
-        abtn.setToolTip('Add a picture from your labtap')
+        self.abtn = QPushButton('Add1', self)
+        self.abtn.setToolTip('Add a picture from your labtap')
+        # self.abtn.clicked.connect(self.openImg)
+        # self.abtn.clicked.connect(self.hide)
+        self.abtn.resize(self.abtn.sizeHint())
+
+        # 创建按钮和提示消息
+        self.adbtn = QPushButton('Add2', self)
+        self.adbtn.setToolTip('Add a picture from internet')
         # abtn.clicked.connect(self.openSlot)
-        abtn.resize(abtn.sizeHint())
+        self.adbtn.resize(self.adbtn.sizeHint())
 
         # 创建quit按钮
         qbtn = QPushButton('Quit', self)
@@ -49,8 +52,9 @@ class pwindow(QWidget):
 
         # 设定按钮布局
         layout = QGridLayout(self)
-        layout.addWidget(abtn, 4, 1, 1, 1)
-        layout.addWidget(qbtn, 4, 2, 1, 1)
+        layout.addWidget(self.abtn, 4, 1, 1, 1)
+        layout.addWidget(self.adbtn, 4, 2, 1, 1)
+        layout.addWidget(qbtn, 4, 3, 1, 1)
         # layout.addWidget(abtn, 4, 1, 1, 1)
 
         self.show()
@@ -66,3 +70,117 @@ class pwindow(QWidget):
         else:
             event.ignore()
 
+
+class cwindow(QWidget):
+
+    def __init__(self):
+
+        # 初始化一个img的ndarray, 用于存储图像
+        self.img = np.ndarray(())
+        super().__init__()
+        self.initUI()
+
+     # 初始化窗口函数
+    def initUI(self):
+
+        # 窗口大小，位置，名称，图标
+        self.setGeometry(300, 300, 1000, 800)
+        self.setWindowTitle('Adding successfully')
+        self.setWindowIcon(QIcon('pic.png'))
+        # self.show()
+
+        self.la = QLabel()
+        self.lrbtn = QPushButton('lr', self)
+        self.rrbtn = QPushButton('rr', self)
+
+        # 创建quit按钮
+        self.qbtn = QPushButton('Quit', self)
+        self.qbtn.setToolTip('Quit this app')
+        self.qbtn.clicked.connect(QCoreApplication.instance().quit)
+
+        # 布局设定
+        layout = QGridLayout(self)
+        layout.addWidget(self.la, 1, 1, 4, 4)
+        layout.addWidget(self.lrbtn, 1, 5, 1, 1)
+        layout.addWidget(self.rrbtn, 2, 5, 1, 1)
+        layout.addWidget(self.qbtn, 4, 5, 1, 1)
+
+    def handle_click(self):
+        if not self.isVisible():
+            self.show()
+
+    def getWeb(self):
+        self.web, okPressed = QInputDialog.getText(self, "Get text", "Web site:", QLineEdit.Normal, "")
+        if okPressed and self.web != '':
+            return self.web
+
+    def getName(self):
+        self.name, okPressed = QInputDialog.getText(self, "Get text", "Img name:", QLineEdit.Normal, "")
+        if okPressed and self.name != '':
+            return self.name
+
+
+    def openImg(self):
+        # 调用打开文件diglog
+        fileName, tmp = QFileDialog.getOpenFileName(
+            self, 'Open Image', './__data', '*.png *.jpg *.bmp')
+
+        if fileName is '':
+            return
+
+        # 采用opencv函数读取数据
+        self.img = cv.imread(fileName, -1)
+
+        if self.img.size == 1:
+            return
+
+        self.refreshShow1()
+
+    def refreshShow1(self):
+
+        # 提取图像的尺寸和通道, 用于将opencv下的image转换成Qimage
+        height, width, channel = self.img.shape
+        bytesPerLine = 3 * width
+        self.qImg = QImage(self.img.data, width, height, bytesPerLine, QImage.Format_RGB888).rgbSwapped()
+
+        # 将Qimage显示出来
+        self.la.setPixmap(QPixmap.fromImage(self.qImg))
+
+    def refreshShow2(self):
+
+        # 提取图像的尺寸和通道, 用于将opencv下的image转换成Qimage
+        height, width, channel = self.webimg.shape
+        bytesPerLine = 3 * width
+        self.qImg = QImage(self.webimg.data, width, height, bytesPerLine, QImage.Format_RGB888).rgbSwapped()
+
+        # 将Qimage显示出来
+        self.la.setPixmap(QPixmap.fromImage(self.qImg))
+
+    def download_img(self):
+        web = self.getWeb()
+        name = self.getName()
+        # self.web, okPressed = QInputDialog.getText(self, "Get text", "Web site:", QLineEdit.Normal, "")
+        # self.name, okPressed = QInputDialog.getText(self, "Get text", "Img name:", QLineEdit.Normal, "")
+        url_info = [web, name]
+        if url_info[1]:
+            # print("-----------downloading image %s" % (url_info[0]))
+            try:
+                url = url_info[0]
+                response = requests.get(url)
+                img = response.content
+
+                # Save Path
+                path = '%s' % (url_info[1])
+                with open(path, 'wb') as f:
+                    f.write(img)
+
+                self.webimg = cv.imread(name, -1)
+                if self.webimg.size == 1:
+                    return
+
+                self.refreshShow2()
+
+                return (True, path)
+            except Exception as ex:
+                print("--------Error----")
+                pass
